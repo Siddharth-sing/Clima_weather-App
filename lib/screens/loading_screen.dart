c0f536cd17947810c7df799fe03b2a3537b7866a
Siddharth Singh Baghel <siddharthsinghrewa5@gmail.com>
2021-06-14 16:18:44 +0530

    Clima Created

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+
+  void getLocation() async {
+
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print(position);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            //Get the current location
+            getLocation();
+          },
+          child: Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
---
+++ scratch.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+void main() {
+  performTasks();
+}
+
+void performTasks() async  {
+  task1();
+  String task2Result = await task2();
+  task3(task2Result);
+}
+
+void task1() {
+
+  String result = 'task 1 data';
+  print('Task 1 complete');
+}
+
+Future task2() async {
+  Duration threeSeconds = Duration(seconds: 3);
+  String result;
+  await Future.delayed(threeSeconds, (){
+    result = 'task 2 data';
+    print('Task 2 complete');
+  });
+  return result;
+}
+
+void task3(String task2result) {
+  String result = task2result;
+  print('Task 3 complete with $result');
+}
