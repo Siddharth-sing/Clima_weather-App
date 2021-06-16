b248c1bd520cde24c998c5edd54f29df17eb420b
Siddharth Singh Baghel <siddharthsinghrewa5@gmail.com>
2021-06-16 22:05:56 +0530

    Networking.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -9,57 +10,30 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  var apiKey = '954631080b4a322c1f51c31455fb0725';
 
   @override
   void initState() {
     super.initState();
   }
 
-  void getLocation() async {
-    Location location = Location();
-    await location.getCurrentLocation();
-  }
-
-  void getData() async {
+  void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
     double lat = location.latitude;
     double lon = location.longitude;
-    http.Response response = await http.get('https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=954631080b4a322c1f51c31455fb0725');
 
-    if(response.statusCode == 200){
-      String data = response.body;
-      print(data);
-      print('\n\n');
+    NetworkHelper networkHelper = NetworkHelper('https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}');
 
-      var decodeData = jsonDecode(data);
-      double longitude = decodeData['coord']['lon'];
-      String weatherDesc = decodeData['weather'][0]['description'];
-      double temp = decodeData['main']['temp'];
-      int pressure = decodeData['main']['pressure'];
-      int humidity = decodeData['main']['humidity'];
-      String country = decodeData['sys']['country'];
-      String timeZone = decodeData['name'];
-
-
-      print(longitude);
-      print(weatherDesc);
-      print(temp);
-      print(pressure);
-      print(humidity);
-      print(country);
-      print(timeZone);
-
-    }else{
-      print(response.statusCode);
-    }
-
+    var weatherReport = networkHelper.getData();
+    print('weather report');
+    print(weatherReport);
   }
 
 
   @override
   Widget build(BuildContext context) {
-    getData();
+    getLocationData();
     return Scaffold();
   }
 }
---
+++ lib/services/networking.dart
@@ -0,0 +1,29 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class NetworkHelper{
+
+  NetworkHelper(this.url);
+
+  final String url;
+
+  Future getData() async {
+
+    http.Response response = await http.get(url);
+
+    if(response.statusCode == 200){
+      String data = response.body;
+      print('data = ${data}');
+      print('\n\n');
+
+      var decodeData = jsonDecode(data);
+
+      return decodeData;
+
+    }else{
+      print(response.statusCode);
+    }
+  }
+
+}
+
